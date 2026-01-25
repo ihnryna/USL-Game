d1f511255e8e2434b37ba17cb0398dbb7822346f
ihnryna <151572051+ihnryna@users.noreply.github.com>
2026-01-25 16:51:43 +0200

    Hand tracking results with mediapipe

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,25 +1,49 @@
 import cv2
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+import time
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 window = "Camera"
 cv2.namedWindow(window)
 
-while True:
-    success, frame = cap.read()
-    if not success:
-        break
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-    cv2.imshow(window, frame)
-    cv2.waitKey(1)
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
 
-    if cv2.getWindowProperty(window, cv2.WND_PROP_VISIBLE) < 1:
-        break
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='model/hand_landmarker.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
-cap.release()
-cv2.destroyAllWindows()
+with HandLandmarker.create_from_options(options) as landmarker:
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        cv2.imshow(window, frame)
+        cv2.waitKey(1)
+
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        mp_image = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=rgb_frame
+        )
+        timestamp = int(time.time() * 1000)
+        landmarker.detect_async(mp_image, timestamp)
+
+        if cv2.getWindowProperty(window, cv2.WND_PROP_VISIBLE) < 1:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
